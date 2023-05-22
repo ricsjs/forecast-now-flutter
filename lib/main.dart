00339b74333861b5442c1434540d7951d10644cd
Ricardo Alencar <ricfilho0007@gmail.com>
2023-05-22 16:24:54 -0300

    teste

--- lib/main.dart
+++ lib/main.dart
@@ -41,7 +41,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'Forecast Now App',
+              'Forecast Now',
             ),
           ],
         ),

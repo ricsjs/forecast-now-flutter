558aa66ef792938fce46ad447e9afdd2b664bd85
Leonardo Bezerra <leonardobezerra05@gmail.com>
2023-05-26 11:53:14 -0300

    feat: changing bottom (#5)

--- lib/main.dart
+++ lib/main.dart
@@ -88,7 +88,7 @@ class NewNavBar extends HookWidget {
         BottomNavigationBarItem(label: "Info", icon: Icon(Icons.info_outline)),
         BottomNavigationBarItem(label: "Home", icon: Icon(Icons.home)),
         BottomNavigationBarItem(
-            label: "Previsão detalhada", icon: Icon(Icons.more))
+            label: "Previsão detalhada", icon: Icon(Icons.filter_drama ))
       ],
     );
   }
